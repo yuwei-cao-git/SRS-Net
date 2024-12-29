4278a2ba91509bb0af5161fdf152dfa29a0e3164
yuwei-cao-git <yuwei.cao94@gmail.com>
2024-12-29 09:58:54 -0800

    'slurm'

--- scripts/ray_img_job.sh
+++ scripts/ray_img_job.sh
@@ -38,9 +38,9 @@ module load python StdEnv gcc arrow
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
-pip install --no-index ray[all]
 pip install --no-index ray[tune] tensorboardX lightning pytorch_lightning torch torchaudio torchdata torcheval torchmetrics torchtext torchvision rasterio imageio wandb numpy pandas
 pip install --no-index scikit-learn seaborn
+pip install mamba-ssm==2.2.2
 # pip install -r requirements.txt
 
 # Set environment variables
@@ -55,12 +55,14 @@ srun python tune_img.py
 
 cd $SLURM_TMPDIR
 tar -cf ~/scratch/tune_img/${next_output_dir}/logs.tar ./tune_img/ray_results/*
-tar -xf ~/scratch/tune_img/${next_output_dir}/logs.tar -C ~/scratch/tune_img/{next_output_dir}
+tar -xf ~/scratch/tune_img/${next_output_dir}/logs.tar -C ~/scratch/tune_img/${next_output_dir}
+
 rm -r ./tune_img/ray_results/
+
 # Check the exit status
 if [ $job_failed -ne 0 ]; then
     echo "Job failed, deleting directory: ${next_output_dir}"
-    # rm -r "${next_output_dir}"
+    rm -r "~/scratch/tune_img/${next_output_dir}"
 else
     echo "Job completed successfully."
 fi

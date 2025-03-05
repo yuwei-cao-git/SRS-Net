c18a20213c88769a1ec552617726fdfb802e7b95
yuwei-cao-git <yuwei.cao94@gmail.com>
2025-03-04 20:10:23 -0800

    'import'

--- models/TransResUnet.py
+++ models/TransResUnet.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
-from ResUnet import ResUnet
+from .ResUnet import ResUnet
 
 # -----------------------------------------------------------------------------------
 # Parts of the fusion module
